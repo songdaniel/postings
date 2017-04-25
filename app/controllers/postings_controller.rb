1705286bdf30f42b018b7bc12b772123d3a6a8f5
Daniel Song <danielzzang5517@likelion.org>
2017-04-25 06:40:25 +0000

    board-complete

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -12,4 +12,24 @@ class PostingsController < ApplicationController
         posting.save
         redirect_to '/index'
     end
+    
+    def update_view
+        @one_post = Posting.find(params[:post_id])
+    end
+    
+    def real
+        @one_post = Posting.find(params[:post_id])
+        @one_post.title = params[:title]
+        @one_post.body = params[:body]
+        
+        @one_post.save
+        redirect_to '/index'
+    end
+    
+    def destroy
+        @one_post = Posting.find(params[:post_id])
+        @one_post.destroy
+        redirect_to '/index'
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,16 @@ Rails.application.routes.draw do
 
   post '/index' => 'postings#create'
   post '/comment/create' => 'comments#create'
+  get 'update_view/:post_id' => 'postings#update_view'
+  get 'destroy/:post_id' => 'postings#destroy'
+  post '/real_update/:post_id' => 'postings#real'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
